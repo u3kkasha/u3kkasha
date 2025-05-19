94089bc5ff7fa77df01bee903619c5fd0aa7a19c
Fida Waseque Choudhury <fida.waseque@gmail.com>
2025-05-19 16:18:26 +0600

    nvim(yank): add 500ms highlight

--- nvim/init.lua
+++ nvim/init.lua
@@ -164,3 +164,10 @@ require("lazy").setup({
     enabled = true
   }
 })
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 500})
+  end,
+})
