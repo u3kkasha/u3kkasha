68ff64b6ab5e6c10a315c03d1763ddbd2544730b
Fida Waseque Choudhury <fida.waseque@gmail.com>
2025-05-24 08:25:12 +0600

    nvim(plugins): enhance mini.ai with additional custom tresitter textobjects

--- nvim/init.lua
+++ nvim/init.lua
@@ -130,12 +130,21 @@ require("lazy").setup({
         require('mini.ai').setup({
           n_lines = 500,
           custom_textobjects = {
-            F = spec_treesitter({ a = '@function.outer', i = '@function.inner' }),
-            r = spec_treesitter({ a = '@return.outer', i = '@return.inner' }),
-            c = spec_treesitter({ a = '@class.outer', i = '@class.inner' }),
+            A = spec_treesitter({ a = '@attribute.outer', i = '@attribute.inner' }),
             C = spec_treesitter({ a = '@comment.outer', i = '@comment.inner' }),
+            F = spec_treesitter({ a = '@function.outer', i = '@function.inner' }),
+            ["="] = spec_treesitter({ a = '@assignment.outer', i = '@assignment.inner' }),
+            ["~"] = spec_treesitter({ a = '@assignment.lhs', i = '@assignment.rhs' }),
+            ["#"] = spec_treesitter({ a = '@number.inner', i = '@number.inner' }),
+            ['@'] = spec_treesitter({ a = '@regex.outer', i = '@regex.innner' }),
+            a = spec_treesitter({ a = '@parameter.outer', i = '@parameter.inner' }),
+            c = spec_treesitter({ a = '@class.outer', i = '@class.inner' }),
+            f = spec_treesitter({ a = '@call.outer', i = '@call.inner' }),
             i = spec_treesitter({ a = '@conditional.outer', i = '@conditional.inner' }),
-            ["="] = spec_treesitter({ a = '@assignment.outer', i = '@assignment.rhs' }),
+            p = spec_treesitter({ a = '@block.outer', i = '@block.inner' }),
+            r = spec_treesitter({ a = '@return.outer', i = '@return.inner' }),
+            l = spec_treesitter({ a = '@loop.outer', i = '@loop.inner' }),
+            s = spec_treesitter({ a = '@statement.outer', i = '@scopename.inner' }),
             g = function()
               local from = { line = 1, col = 1 }
               local to = {
