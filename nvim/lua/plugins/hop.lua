d29c7f92e888463410f44f7da4e16a9aa9ac7186
Fida Waseque Choudhury <fida.waseque@gmail.com>
2025-06-06 06:32:58 +0600

    nvim(plugins): update hop.nvim version and configuration settings

--- nvim/lua/plugins/hop.lua
+++ nvim/lua/plugins/hop.lua
@@ -1,10 +1,12 @@
 -- بسم الله الرحمن الرحيم
 return {
     'smoka7/hop.nvim',
-    version = "*",
-    lazy = true,
+    version = "v2.7.2",
+    lazy = false,
     keys = {
-        { '<leader><leader>w', '<CMD>HopWord<CR>', desc = 'Jump to any word', mode = 'n' }
+        { '<leader><leader>w', '<CMD>HopWord<CR>', desc = 'Jump to any word', mode = 'n' },
+        { '<leader><leader>y', '<CMD>HopYankChar1<CR>', desc = 'Yank the text between two hinted position without jumping', mode = 'n' },
+        { '<leader><leader>n', '<CMD>HopNodes<CR>', desc = 'Jump to any node', mode = 'n' },
     },
     opts = {
         keys = 'etovxqpdygfblzhckisuran'
