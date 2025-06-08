0a9174e8833ca25f8ba287f9d5264389bd42c224
Fida Waseque Choudhury <fida.waseque@gmail.com>
2025-06-08 18:11:35 +0600

    nvim(plugins): add git stage/unstage shortcuts

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -5,6 +5,8 @@ if vim.g.vscode then
     local function map(mode, keybinding, actionCommand)
         vim.keymap.set(mode, keybinding, function() vscode.call(actionCommand) end, { silent = true, noremap = true })
     end
+
+    -- Folding
     map('n', 'zC', 'editor.foldRecursively')
     map('n', 'zM', 'editor.foldAll')
     map('n', 'zO', 'editor.unfoldRecursively')
@@ -21,13 +23,16 @@ if vim.g.vscode then
     map('n', '<leader>.', 'breadcrumbs.focusAndSelect')
     map('n', '<leader>o', 'outline.focus')
     map('n', '<leader>f', 'workbench.view.explorer')
-
     vim.keymap.set({ "n", "x" }, "<leader>r", function()
         vscode.with_insert(function()
             vscode.action("editor.action.refactor")
         end)
     end)
+    map({'n', 'v'}, '<leader>gr', 'git.revertSelectedRanges')
+    map({'n', 'v'}, '<leader>gs','git.stageSelectedRanges')
+    map({'n', 'v'}, '<leader>gS','git.unstageSelectedRanges')
 
+    -- Navigation
     map('n', ']h', "editor.action.dirtydiff.next")
     map('n', '[h', "editor.action.dirtydiff.previous")
     map('n', '[d', 'editor.action.marker.next')
