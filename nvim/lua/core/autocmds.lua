824305cfc7baaadef4179739be4ce2b06fc59e55
Fida Waseque Choudhury <fida.waseque@gmail.com>
2025-07-26 12:18:20 +0600

    feat(autocmds): add autocmd to disable automatic comment insertion
    Added an autocmd to prevent automatic comment insertion for all file types, enhancing user control over formatting.

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -12,3 +12,9 @@ vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "TermNormal", { bg = "none" })
 vim.api.nvim_set_hl(0, "TermNormalNC", { bg = "none" })
+
+-- Disable automatic comment insertion for all file types
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = '*',
+  command = 'set formatoptions-=cro',
+})
