85fce208ca6a31a804739cbf640aa9dcc5b2e547
Fida Waseque Choudhury <fida.waseque@gmail.com>
2025-11-27 06:48:59 +0600

    fix(nvim/autocmds): use augroup to avoid duplicate autocommands

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -1,7 +1,10 @@
 -- بسم الله الرحمن الرحيم
 
+local augroup = vim.api.nvim_create_augroup('UserAutocmds', { clear = true })
+
 -- Highlight on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
+  group = augroup,
   callback = function()
     vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 500})
   end,
@@ -15,6 +18,7 @@ vim.api.nvim_set_hl(0, "TermNormalNC", { bg = "none" })
 
 -- Disable automatic comment insertion for all file types
 vim.api.nvim_create_autocmd('FileType', {
+  group = augroup,
   pattern = '*',
   command = 'set formatoptions-=cro',
 })
