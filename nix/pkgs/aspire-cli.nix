0767a0c3e4e94d2bf753acb6d85aa82d192b7a02
Fida Waseque Choudhury <fida.waseque@gmail.com>
2026-05-10 06:16:24 +0000

    feat(nix): modularize aspire-cli and enhance system config

---
+++ nix/pkgs/aspire-cli.nix
@@ -0,0 +1,19 @@
+{ pkgs }:
+
+pkgs.stdenv.mkDerivation {
+  pname = "aspire-cli";
+  version = "13.3.0";
+  src = pkgs.fetchurl {
+    url = "https://aka.ms/dotnet/9/aspire/ga/daily/aspire-cli-linux-x64.tar.gz";
+    sha256 = "1l2df2h4psy42j9pyg8fhanxfahs25vn0gvpa0n7binz18z3szpp";
+  };
+  nativeBuildInputs = [ pkgs.autoPatchelfHook pkgs.makeWrapper ];
+  buildInputs = with pkgs; [ stdenv.cc.cc.lib zlib icu openssl ];
+  sourceRoot = ".";
+  installPhase = ''
+    mkdir -p $out/bin
+    cp aspire $out/bin/
+    wrapProgram $out/bin/aspire \
+      --prefix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath [ pkgs.icu pkgs.openssl ]}
+  '';
+}
