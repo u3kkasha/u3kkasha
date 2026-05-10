ea2824fb36f8a6f89d5f5e4af28573ac1723af3d
Fida Waseque Choudhury <fida.waseque@gmail.com>
2026-05-10 05:36:58 +0000

    feat(podman): add podman module and configure user groups

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -4,6 +4,7 @@
   imports = [
     ./modules/wsl.nix
     ./modules/system.nix
+    ./modules/podman.nix
     ./modules/home-manager.nix
   ];
 }
--- nix/modules/system.nix
+++ nix/modules/system.nix
@@ -1,5 +1,18 @@
 { pkgs, ... }:
 
+      sha256 = "1l2df2h4psy42j9pyg8fhanxfahs25vn0gvpa0n7binz18z3szpp";
+    };
+    nativeBuildInputs = [ pkgs.autoPatchelfHook pkgs.makeWrapper ];
+    buildInputs = with pkgs; [ stdenv.cc.cc.lib zlib icu openssl ];
+    sourceRoot = ".";
+    installPhase = ''
+      mkdir -p $out/bin
+      cp aspire $out/bin/
+      wrapProgram $out/bin/aspire \
+        --prefix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath [ pkgs.icu pkgs.openssl ]}
+    '';
+  };
+in
 {
   nix.settings.experimental-features = [
     "nix-command"
@@ -21,6 +34,7 @@
   users.users.nixos = {
     isNormalUser = true;
     shell = pkgs.nushell;
+    extraGroups = [ "podman" ];
   };
 
   programs.nix-ld.enable = true;
