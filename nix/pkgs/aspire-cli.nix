dc13a12907fe5b3ff15da48356482ef4b2bbb8dc
Fida Waseque Choudhury <fida.waseque@gmail.com>
2026-05-10 08:11:14 +0000

    style(nix): format aspire-cli.nix
    Standardizes formatting and indentation in the aspire-cli derivation.

--- nix/pkgs/aspire-cli.nix
+++ nix/pkgs/aspire-cli.nix
@@ -7,13 +7,26 @@ pkgs.stdenv.mkDerivation {
     url = "https://aka.ms/dotnet/9/aspire/ga/daily/aspire-cli-linux-x64.tar.gz";
     sha256 = "1l2df2h4psy42j9pyg8fhanxfahs25vn0gvpa0n7binz18z3szpp";
   };
-  nativeBuildInputs = [ pkgs.autoPatchelfHook pkgs.makeWrapper ];
-  buildInputs = with pkgs; [ stdenv.cc.cc.lib zlib icu openssl ];
+  nativeBuildInputs = [
+    pkgs.autoPatchelfHook
+    pkgs.makeWrapper
+  ];
+  buildInputs = with pkgs; [
+    stdenv.cc.cc.lib
+    zlib
+    icu
+    openssl
+  ];
   sourceRoot = ".";
   installPhase = ''
     mkdir -p $out/bin
     cp aspire $out/bin/
     wrapProgram $out/bin/aspire \
-      --prefix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath [ pkgs.icu pkgs.openssl ]}
+      --prefix LD_LIBRARY_PATH : ${
+        pkgs.lib.makeLibraryPath [
+          pkgs.icu
+          pkgs.openssl
+        ]
+      }
   '';
 }
