f1f9e790f1b101819a552595485b1e1d1b7aaacb
Fida Waseque Choudhury <fida.waseque@gmail.com>
2026-05-17 08:15:48 +0000

    feat(home): set oh-my-posh theme to catppuccin_mocha

--- nix/modules/home/cli.nix
+++ nix/modules/home/cli.nix
@@ -3,5 +3,8 @@ _:
 {
   programs.carapace.enable = true;
   programs.television.enable = true;
-  programs.oh-my-posh.enable = true;
+  programs.oh-my-posh = {
+    enable = true;
+    useTheme = "catppuccin_mocha";
+  };
 }
