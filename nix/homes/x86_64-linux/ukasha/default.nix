ea659fc83be9eb603f7b83b5e6431075163d2a77
Fida Waseque Choudhury <fida.waseque@gmail.com>
2026-05-29 14:04:28 +0600

    refactor(home): fix duplicate module imports for snowfall-lib

--- nix/homes/x86_64-linux/ukasha/default.nix
+++ nix/homes/x86_64-linux/ukasha/default.nix
@@ -1,10 +1,5 @@
-{ ... }:
+_:
 
 {
-  imports = [
-    # Import the main home module which includes all others
-    # Since this file is in homes/x86_64-linux/ukasha/default.nix,
-    # the path to modules/home/default.nix is ../../../modules/home/default.nix
-    ../../../modules/home/default.nix
-  ];
+  # Modules in modules/home are automatically imported by snowfall-lib
 }
