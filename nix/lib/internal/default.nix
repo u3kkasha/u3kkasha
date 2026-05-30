31eeb2f9963cad39eb25ac2efc5569af7c133a08
Fida Waseque Choudhury <fida.waseque@gmail.com>
2026-05-30 10:55:42 +0600

    chore(lib): track internal library

---
+++ nix/lib/internal/default.nix
@@ -0,0 +1,12 @@
+_:
+
+{
+  username = "ukasha";
+  name = "Fida Waseque Choudhury";
+  email = "fida.waseque@gmail.com";
+  defaultEditor = "hx";
+  defaultTerminal = "ghostty";
+  themeFlavor = "mocha";
+  systemStateVersion = "25.11";
+  homeStateVersion = "25.11";
+}
