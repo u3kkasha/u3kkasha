a74f6237511ffa38ac9d6f8d3991c953461094bb
Fida Waseque Choudhury <fida.waseque@gmail.com>
2026-05-30 12:59:42 +0600

    config(wsl): disable GUI modules for WSL host

--- nix/systems/x86_64-linux/nixos-wsl/default.nix
+++ nix/systems/x86_64-linux/nixos-wsl/default.nix
@@ -7,6 +7,8 @@
     wsl.enable = true;
   };
 
+  home-manager.users.${lib.internal.username}.internal.gui.enable = false;
+
   snowfallorg.users.ukasha.home.enable = true;
 
   networking.hostName = "nixos-wsl";
