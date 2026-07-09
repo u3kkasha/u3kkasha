84d9530725b22cda0afeedc400691885959eb52e
Fida Waseque Choudhury <fida.waseque@gmail.com>
2026-07-09 17:50:38 +0600

    fix(nix): 🔧 trust configured binary caches

--- nix/flake.nix
+++ nix/flake.nix
@@ -22,18 +22,6 @@
     hunk.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-      "https://cache.numtide.com"
-      "https://noctalia.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
-      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
-    ];
-  };
   outputs =
     inputs:
     let
--- nix/modules/nixos/system/default.nix
+++ nix/modules/nixos/system/default.nix
@@ -29,6 +29,16 @@ in
       trusted-users = [
         "root"
       ];
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://cache.numtide.com"
+        "https://noctalia.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+        "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+      ];
       min-free = 5 * 1024 * 1024 * 1024; # 5GB
       max-free = 10 * 1024 * 1024 * 1024; # 10GB
     };
