a2485010acf41cbe373bd25feaa5f5214687d714
Fida Waseque Choudhury <fida.waseque@gmail.com>
2026-07-24 18:26:31 +0600

    fix(cache): 🔧 add trusted binary caches to nixConfig

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,5 +1,19 @@
 {
   description = "Multi-System Nix Flake";
+
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.numtide.com"
+      "https://noctalia.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
